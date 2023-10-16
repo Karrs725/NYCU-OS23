b196b91b24570c33d88d4b5a6c3e6bbd8508f77a
Karrs725 <karrs600914@gmail.com>
2023-10-16 10:44:47 +0800

    Lab2

---
+++ Lab2/hw2_110550063.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int N, M;
+    vector<vector<int>> queues;
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        int mode, time_quantum;
+        vector<int> q;
+        cin >> mode >> time_quantum;
+        q.push_back(mode);
+        q.push_back(time_quantum);
+        queues.push_back(q);
+    }
+    for (int i = 0; i < M; i++)
+    {
+        
+    }
+    
+    
+}
