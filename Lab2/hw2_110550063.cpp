2f1b739c21b9990bd8cb4e8f4f2bdfa94f868d5d
Karrs725 <karrs600914@gmail.com>
2023-10-23 14:20:19 +0800

    hw2

--- Lab2/hw2_110550063.cpp
+++ Lab2/hw2_110550063.cpp
@@ -5,8 +5,9 @@ struct Process{
     int arrival_time;
     int bust_time;
     int remain_time;
-    int wait_time;
+    int wait_time = 0;
     int turnaround_time;
+    int complete_time;
 };
 
 struct Queue{
@@ -15,10 +16,28 @@ struct Queue{
     vector<int> process_in_queue;
 };
 
+int FCFS(vector<Queue> &q, vector<Process> &p, int num_of_process){
+    bool end = false;
+    
+    int time = 0, id = 0, total_wait = 0;
+    for (int i = 0; i < num_of_process; i++)
+    {
+        total_wait += p[i].wait_time;
+    }
+    
+    while (!end){
+        if (p[id].arrival_time == time){
+            p[id].wait_time = p[id];
+            id++;
 
+        }
+        
+    }
+}
 
 int main(){
-    int N, M, time = 0;
+    int N, M, global_time = 0, id = 0;
+    bool end = false;
     vector<Queue> queues;
     vector<Process> processes;
     cin >> N >> M;
@@ -30,15 +49,19 @@ int main(){
     for (int i = 0; i < M; i++){
         Process p;
         cin >> p.arrival_time >> p.bust_time;
+        p.remain_time = p.bust_time;
         processes.push_back(p);
     }
 
-    if (M == 1)
+    
+    
+
+    if (N == 1)
     {
         switch (queues[0].mode)
         {
         case 0:
-            /* code */
+            FCFS();
             break;
         case 1:
 
